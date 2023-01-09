a05aefe92830906217d51096f2587f2fe720fb46
lidnaldo <33027833+lidnaldo@users.noreply.github.com>
2023-01-09 11:32:45 -0300

    Add arquivos via upload

---
+++ Hello GitHub/src/br/com/lidnaldo/git/HelloGit.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package br.com.lidnaldo.git;
+
+/**
+ * @author Lidnaldo
+ * Exemplo de integração do Eclipse com o GitHub
+ *
+ */
+public class HelloGit {
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+	}
+
+}
